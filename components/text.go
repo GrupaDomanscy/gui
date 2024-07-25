a4aab8d1d5c7b5718f5324145a88839c234581c6
Bartłomiej 'itedya' Domański <bdomanski@domanscy.group>
2024-07-25 22:15:01 +0200

    Text wrapping, part 1

--- components/text.go
+++ components/text.go
@@ -2,6 +2,7 @@ package components
 
 import (
 	"fmt"
+	"strings"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -9,23 +10,66 @@ import (
 type TextComponent struct {
 	Component
 
-	text     string
-	fontName string
-	fontSize float32
-	spacing  float32
-	color    rl.Color
-	position ComponentPosition
+	text          string
+	processedText string
+	wrapText      bool
+	fontName      string
+	fontSize      float32
+	spacing       float32
+	color         rl.Color
+	position      ComponentPosition
 }
 
 func NewTextComponent(text string, loadedFontName string, fontSize float32, spacing float32, color rl.Color) *TextComponent {
 	return &TextComponent{
-		text:     text,
-		fontName: loadedFontName,
-		fontSize: fontSize,
-		spacing:  spacing,
-		color:    color,
-		position: NewComponentPosition(),
+		text:          text,
+		processedText: "",
+		wrapText:      false,
+		fontName:      loadedFontName,
+		fontSize:      fontSize,
+		spacing:       spacing,
+		color:         color,
+		position:      NewComponentPosition(),
 	}
+}
+
+func getGlyphWidth(font *rl.Font, glyph int32, fontSize float32, cachedFontScalingFactor float32) float32 {
+	var fontScalingFactor float32
+
+	if cachedFontScalingFactor == nil {
+		fontScalingFactor = float32(font.BaseSize) / fontSize
+	} else {
+		fontScalingFactor := *cachedFontScalingFactor
+	}
+
+	glyphInfo := rl.GetGlyphInfo(*font, glyph)
+
+	if glyphInfo.AdvanceX != 0 {
+		return float32(glyphInfo.AdvanceX)
+	} else {
+		return float32(glyphInfo.AdvanceX)
+	}
+}
+
+func wrapText(font rl.Font, text string, fontSize float32, spacing float32, maxWidth float32) (processedText string, calculatedSize rl.Vector2) {
+	fontScalingFactor := float32(font.BaseSize) / fontSize
+
+	lastWhitespaceCharacterIndex := -1
+	var currentLineText strings.Builder
+	var currentLineWidth float32 = 0
+	var defaultLineHeight float32 = fontSize // this is bullshit, we have for ex. arabic characters which break this rule
+
+	for characterIndex, character := range text {
+		if character == ' ' {
+			lastWhitespaceCharacterIndex = characterIndex
+		}
+
+		currentLineText.WriteRune(character)
+
+		currentLineWidth += glyphWidth
+	}
+
+	return
 }
 
 func (comp *TextComponent) SetPosition(pos rl.Vector2) {
@@ -42,7 +86,15 @@ func (comp *TextComponent) CalculateSize(getFont GetFontCallback, maxViewport rl
 		panic(fmt.Sprintf("Provided font (%s) is not loaded into memory", comp.fontName))
 	}
 
-	return rl.MeasureTextEx(font, comp.text, comp.fontSize, comp.spacing)
+	if !comp.wrapText {
+		return rl.MeasureTextEx(font, comp.text, comp.fontSize, comp.spacing)
+	} else {
+		var calculatedSize rl.Vector2
+
+		comp.processedText, calculatedSize = wrapText(font, comp.text, comp.fontSize, comp.spacing, maxViewport.X)
+
+		return calculatedSize
+	}
 }
 
 func (comp *TextComponent) Render(getFont GetFontCallback) {
