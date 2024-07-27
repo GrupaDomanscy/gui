3b2a85bceca58e6f9f0a09dbdbb111bd7d2ca0f2
Bartłomiej 'itedya' Domański <bdomanski@domanscy.group>
2024-07-27 11:43:26 +0200

    add simple text wrapping feature

--- components/text.go
+++ components/text.go
@@ -33,41 +33,101 @@ func NewTextComponent(text string, loadedFontName string, fontSize float32, spac
 	}
 }
 
-func getGlyphWidth(font *rl.Font, glyph int32, fontSize float32, cachedFontScalingFactor float32) float32 {
-	var fontScalingFactor float32
+func (component *TextComponent) SetWrapText(wrap bool) {
+	component.wrapText = wrap
+}
 
-	if cachedFontScalingFactor == nil {
-		fontScalingFactor = float32(font.BaseSize) / fontSize
-	} else {
-		fontScalingFactor := *cachedFontScalingFactor
-	}
-
+func getGlyphWidth(font *rl.Font, glyph int32) float32 {
 	glyphInfo := rl.GetGlyphInfo(*font, glyph)
 
 	if glyphInfo.AdvanceX != 0 {
 		return float32(glyphInfo.AdvanceX)
 	} else {
-		return float32(glyphInfo.AdvanceX)
+		return (*font).Recs.Width + float32(glyphInfo.OffsetX)
 	}
 }
 
 func wrapText(font rl.Font, text string, fontSize float32, spacing float32, maxWidth float32) (processedText string, calculatedSize rl.Vector2) {
+	type IterationState struct {
+		wrappedText                  strings.Builder
+		currentLineText              strings.Builder
+		currentLineWidth             float32
+		defaultLineHeight            float32 // this is bullshit, we have for ex. arabic characters which break this rule
+		lastWhitespaceCharacterIndex int
+	}
+
 	fontScalingFactor := float32(font.BaseSize) / fontSize
 
-	lastWhitespaceCharacterIndex := -1
-	var currentLineText strings.Builder
-	var currentLineWidth float32 = 0
-	var defaultLineHeight float32 = fontSize // this is bullshit, we have for ex. arabic characters which break this rule
+	processEndOfInput := func(state *IterationState) {
+		state.wrappedText.WriteString(state.currentLineText.String())
+		calculatedSize.Y += state.defaultLineHeight
 
-	for characterIndex, character := range text {
-		if character == ' ' {
-			lastWhitespaceCharacterIndex = characterIndex
+		if state.currentLineWidth > calculatedSize.X {
+			calculatedSize.X = state.currentLineWidth * fontScalingFactor
+		}
+	}
+
+	processEndOfLine := func(state *IterationState) {
+		if state.currentLineWidth > calculatedSize.X {
+			calculatedSize.X = state.currentLineWidth * fontScalingFactor
 		}
 
-		currentLineText.WriteRune(character)
+		state.wrappedText.WriteString(state.currentLineText.String())
+		state.currentLineText.Reset()
 
-		currentLineWidth += glyphWidth
+		state.currentLineWidth = 0
+		state.currentLineText.WriteRune('\n')
+
+		calculatedSize.Y += state.defaultLineHeight
 	}
+
+	previousCharacterState := IterationState{
+		lastWhitespaceCharacterIndex: -1,
+		currentLineWidth:             0,
+		defaultLineHeight:            fontSize,
+	}
+
+	state := IterationState{
+		lastWhitespaceCharacterIndex: -1,
+		currentLineWidth:             0,
+		defaultLineHeight:            fontSize,
+	}
+
+	runes := []rune(text)
+
+	for characterIndex := 0; characterIndex < len(runes); characterIndex++ {
+		character := runes[characterIndex]
+
+		previousCharacterState = state
+
+		if character == ' ' {
+			state.lastWhitespaceCharacterIndex = characterIndex
+		}
+
+		state.currentLineText.WriteRune(character)
+
+		if character == '\n' {
+			processEndOfLine(&state)
+		}
+
+		glyphWidth := getGlyphWidth(&font, character)
+		state.currentLineWidth += glyphWidth
+
+		if state.currentLineWidth > maxWidth {
+			if state.currentLineText.Len() == 1 {
+				panic("Length of state.currentLineText is equal to 1, unhandled variant.")
+			}
+
+			state = previousCharacterState
+			characterIndex -= 1
+
+			processEndOfLine(&state)
+		}
+	}
+
+	processEndOfInput(&state)
+
+	processedText = state.wrappedText.String()
 
 	return
 }
@@ -86,11 +146,14 @@ func (comp *TextComponent) CalculateSize(getFont GetFontCallback, maxViewport rl
 		panic(fmt.Sprintf("Provided font (%s) is not loaded into memory", comp.fontName))
 	}
 
-	if !comp.wrapText {
-		return rl.MeasureTextEx(font, comp.text, comp.fontSize, comp.spacing)
-	} else {
-		var calculatedSize rl.Vector2
+	var calculatedSize rl.Vector2
 
+	if !comp.wrapText {
+		calculatedSize = rl.MeasureTextEx(font, comp.text, comp.fontSize, comp.spacing)
+		comp.processedText = comp.text
+
+		return calculatedSize
+	} else {
 		comp.processedText, calculatedSize = wrapText(font, comp.text, comp.fontSize, comp.spacing, maxViewport.X)
 
 		return calculatedSize
@@ -103,7 +166,7 @@ func (comp *TextComponent) Render(getFont GetFontCallback) {
 		panic(fmt.Sprintf("Provided font (%s) is not loaded into memory", comp.fontName))
 	}
 
-	rl.DrawTextEx(font, comp.text, comp.position.Calculate(), comp.fontSize, comp.spacing, comp.color)
+	rl.DrawTextEx(font, comp.processedText, comp.position.Calculate(), comp.fontSize, comp.spacing, comp.color)
 }
 
 func (text *TextComponent) GetPosition() rl.Vector2 {
