bf75b340eef43ef94c0b1e2cfba5a2a0eaf9442f
Bartłomiej 'itedya' Domański <bdomanski@domanscy.group>
2024-07-29 21:57:45 +0200

    Change clock values functions to be pointer funcs

--- components/atoms/clock_values.go
+++ components/atoms/clock_values.go
@@ -11,42 +11,42 @@ func NewClockValues() ClockValues {
 	return ClockValues{0, 0, 0, 0}
 }
 
-func (c ClockValues) Top() float32 {
+func (c *ClockValues) Top() float32 {
 	return c.top
 }
 
-func (c ClockValues) Bottom() float32 {
+func (c *ClockValues) Bottom() float32 {
 	return c.bottom
 }
 
-func (c ClockValues) Left() float32 {
+func (c *ClockValues) Left() float32 {
 	return c.left
 }
 
-func (c ClockValues) Right() float32 {
+func (c *ClockValues) Right() float32 {
 	return c.right
 }
 
-func (c ClockValues) SetTop(value float32) {
+func (c *ClockValues) SetTop(value float32) {
 	c.top = value
 }
 
-func (c ClockValues) SetBottom(value float32) {
+func (c *ClockValues) SetBottom(value float32) {
 	c.bottom = value
 }
 
-func (c ClockValues) SetLeft(value float32) {
+func (c *ClockValues) SetLeft(value float32) {
 	c.left = value
 }
 
-func (c ClockValues) SetRight(value float32) {
+func (c *ClockValues) SetRight(value float32) {
 	c.right = value
 }
 
-func (c ClockValues) HorizontalSum() float32 {
+func (c *ClockValues) HorizontalSum() float32 {
 	return c.left + c.right
 }
 
-func (c ClockValues) VerticalSum() float32 {
+func (c *ClockValues) VerticalSum() float32 {
 	return c.top + c.bottom
 }
