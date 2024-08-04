055f8165fe489e72451b69c9ca37cc93131cde94
Bartłomiej 'itedya' Domański <bdomanski@domanscy.group>
2024-08-04 10:16:21 +0200

    Add cursor to input

--- components/input.go
+++ components/input.go
@@ -1,12 +1,11 @@
 package components
 
 import (
-	"fmt"
-	"slices"
-
 	"domanscy.group/gui/components/atoms"
 	"domanscy.group/gui/components/events"
+	"fmt"
 	rl "github.com/gen2brain/raylib-go/raylib"
+	"slices"
 )
 
 type InputComponent struct {
@@ -22,7 +21,13 @@ type InputComponent struct {
 	backgroundColor rl.Color
 	textColor       rl.Color
 
-	text string
+	text              string
+	textGlyphHitboxes []rl.Rectangle
+
+	cursorRunePos uint
+	cursorXPos    float32
+
+	isInitialized bool
 }
 
 func NewInputComponent(eventBus *atoms.EventBus, fontName string, fontSize float32, maxWidth float32, backgroundColor rl.Color, textColor rl.Color) *InputComponent {
@@ -42,31 +47,80 @@ func NewInputComponent(eventBus *atoms.EventBus, fontName string, fontSize float
 		backgroundColor: backgroundColor,
 		textColor:       textColor,
 
-		text: "",
+		text:              "",
+		textGlyphHitboxes: []rl.Rectangle{},
+
+		cursorRunePos: 0,
+		cursorXPos:    0,
+
+		isInitialized: false,
 	}
 
-	eventBus.ListenToEvent("gui:keyaction", func(rawArgs ...interface{}) {
+	return component
+}
+
+func convertGlyphInfoToRectangle(info rl.GlyphInfo, font rl.Font, fontSize float32) rl.Rectangle {
+	return rl.Rectangle{
+		X:      0,
+		Y:      0,
+		Width:  float32(info.AdvanceX) * float32(font.BaseSize) / fontSize,
+		Height: fontSize,
+	}
+}
+
+func (component *InputComponent) assignEventListeners(getFont GetFontCallback) {
+	component.eventBus.ListenToEvent("gui:keyaction", func(rawArgs ...interface{}) {
 		args := rawArgs[0].(events.KeyPressEvent)
 
 		textInRunes := []rune(component.text)
 
-		if slices.Contains(args.PressedKeys, rl.KeyBackspace) {
+		if slices.Contains(args.PressedKeys, rl.KeyLeft) {
+			component.cursorRunePos--
+
+			if component.cursorRunePos < 0 {
+				component.cursorRunePos = 0
+			}
+		} else if slices.Contains(args.PressedKeys, rl.KeyRight) {
+			component.cursorRunePos++
+
+			if component.cursorRunePos > uint(len(textInRunes)) {
+				component.cursorRunePos = uint(len(textInRunes))
+			}
+		} else if slices.Contains(args.PressedKeys, rl.KeyBackspace) {
 			if len(textInRunes) == 0 {
 				return
 			}
 
+			component.cursorXPos -= component.textGlyphHitboxes[len(component.textGlyphHitboxes)-1].Width
+			component.textGlyphHitboxes = component.textGlyphHitboxes[:len(component.textGlyphHitboxes)-1]
 			textInRunes = textInRunes[:len(textInRunes)-1]
+			component.cursorRunePos--
 		} else {
+			font, err := getFont(component.fontName)
+			if err != nil {
+				panic("Font has not been loaded into memory")
+			}
+
+			for _, pressedChar := range args.PressedChars {
+				hitbox := convertGlyphInfoToRectangle(rl.GetGlyphInfo(font, pressedChar), font, component.fontSize)
+				component.textGlyphHitboxes = append(component.textGlyphHitboxes, hitbox)
+				component.cursorXPos += hitbox.Width
+				component.cursorRunePos++
+			}
+
 			textInRunes = append(textInRunes, args.PressedChars...)
 		}
 
 		component.text = string(textInRunes)
 	})
-
-	return component
 }
 
 func (input *InputComponent) CalculateSize(getFont GetFontCallback, maxViewport rl.Vector2) rl.Vector2 {
+	if !input.isInitialized {
+		input.assignEventListeners(getFont)
+		input.isInitialized = true
+	}
+
 	var width float32 = 0
 
 	if maxViewport.X < input.maxWidth {
@@ -96,7 +150,9 @@ func (input *InputComponent) Render(getFont GetFontCallback) {
 
 	rl.DrawRectangle(int32(position.X), int32(position.Y), int32(size.X), int32(size.Y), input.backgroundColor)
 
-	rl.DrawTextEx(font, input.text, input.position.Calculate(), input.fontSize, 0, input.textColor)
+	rl.DrawTextEx(font, input.text, position, input.fontSize, 0, input.textColor)
+
+	rl.DrawRectangle(int32(input.cursorXPos), int32(position.Y), 2, int32(input.fontSize), input.textColor)
 }
 
 func (input *InputComponent) SetPosition(pos rl.Vector2) {
